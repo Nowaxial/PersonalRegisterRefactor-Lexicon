f3ec79e01a5e84c6172c9d81a8ebe2576ce61947
Nowaxial <Nowaxial@users.noreply.github.com>
2025-04-02 20:01:38 +0200

    Added "first name", "last name" and "salary"

--- PersonalRegisterRefactor/Employee.cs
+++ PersonalRegisterRefactor/Employee.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace PersonalRegisterRefactor
 {
-    internal class Employees
+    internal class Employee(string firstName, string lastName, int salary)
     {
+        public string FirstName { get; set; } = firstName;
+        public string LastName { get; set; } = lastName;
+        public int Salary { get; set; } = salary;
     }
 }
