a58fdfd29467f512bbf46e4c164d8d30f70e746b
Nowaxial <Nowaxial@users.noreply.github.com>
2025-04-02 17:36:14 +0200

    Add project files.

---
+++ PersonalRegisterRefactor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PersonalRegisterRefactor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
