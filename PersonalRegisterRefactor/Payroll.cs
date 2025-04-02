0016847673aa62c77a621119ba81ab422767f2b7
Nowaxial <Nowaxial@users.noreply.github.com>
2025-04-02 20:28:43 +0200

    Added AddEmployee(), GetEmployees , Employee List and Playroll

--- PersonalRegisterRefactor/Payroll.cs
+++ PersonalRegisterRefactor/Payroll.cs
@@ -8,5 +8,21 @@ namespace PersonalRegisterRefactor
 {
     internal class Payroll
     {
+        private List<Employee> employees;
+
+        public Payroll()
+        {
+            employees = [];
+        }
+
+        internal void AddEmployee(string firstName, string lastName, int salary)
+        {
+            employees.Add(new Employee(firstName, lastName, salary));
+        }
+
+        internal IEnumerable<Employee> GetEmployees()
+        {
+            return employees;
+        }
     }
 }
